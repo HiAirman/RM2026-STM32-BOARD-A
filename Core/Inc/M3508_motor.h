d287ed517760f3b7af9780e1a1697f1d0c62753c
Airman Han <hiairman@outlook.com>
2025-10-21 17:41:45 +0800

    feat:
    - M3508_motor.cpp modified CanRxMsgCallBack(), convert units.

--- Core/Inc/M3508_motor.h
+++ Core/Inc/M3508_motor.h
@@ -4,25 +4,25 @@
 
 #ifndef RM_A_BOARD_TEST_DEV_M3508_MOTOR_H
 #define RM_A_BOARD_TEST_DEV_M3508_MOTOR_H
-#include "stm32f4xx_hal.h"
+
+#include "main.h"
 
 class M3508_Motor {
-private :
-    const float kratio_ = 0.f;                //电机减速比
-
-    float angle_ = 0.f;                 // deg 输出端累计转动角度
-    float delta_angle_ = 0.f;           // deg 输出端新转动角度
-    float ecd_angle_ = 0.f;             // deg 当前电机编码器角度
-    float last_ecd_angle_ = 0.f;        // deg 上次电机编码器角度
-    float delta_ecd_angle_ = 0.f;       // deg 编码器端新转动角度
-    float rotate_speed_ = 0.f;          // dps 反馈转子转速
-    float current_ = 0.f;               // A 反馈转矩电流
-    float temp_ = 0.f;                  // ℃ 反馈电机角度
-
 public:
-    explicit M3508_Motor( float kratio);
-    void canRxMsgCallBack(const uint8_t rx_data_[8]);
+    explicit M3508_Motor(float kratio);
+    void CanRxMsgCallBack(const uint8_t rx_data_[8]);
 
+private :
+    const float kratio_ = 0.f; //电机减速比
+
+    float angle_ = 0.f; // deg 输出端累计转动角度
+    float delta_angle_ = 0.f; // deg 输出端新转动角度
+    float ecd_angle_ = 0.f; // deg 当前电机编码器角度
+    float last_ecd_angle_ = 0.f; // deg 上次电机编码器角度
+    float delta_ecd_angle_ = 0.f; // deg 编码器端新转动角度
+    float rotate_speed_ = 0.f; // dps 反馈转子转速
+    float current_ = 0.f; // A 反馈转矩电流
+    float temp_ = 0.f; // ℃ 反馈电机角度
 };
 
 extern M3508_Motor motor;
--- Core/Src/M3508_motor.cpp
+++ Core/Src/M3508_motor.cpp
@@ -9,15 +9,14 @@ extern uint16_t speed;
 extern uint16_t current;
 extern uint8_t temperature;
 
-M3508_Motor::M3508_Motor(const float kratio) : kratio_(kratio)
-{}
+M3508_Motor::M3508_Motor(const float kratio):
+  kratio_(kratio) {}
 
-void M3508_Motor::canRxMsgCallBack(const uint8_t rx_data_[8]){
-    angle = (rx_data_[0] << 8) | rx_data_[1];
-    speed = (rx_data_[2] << 8) | rx_data_[3];
-    current = (rx_data_[4] << 8) | rx_data_[5];
-    temperature = rx_data_[6];
-
+void M3508_Motor::CanRxMsgCallBack(const uint8_t rx_data_[8]) {
+  angle = (int16_t)((rx_data_[0] << 8) | rx_data_[1]) * 360.f / 8191.f;  //单位 °
+  speed = (int16_t)((rx_data_[2] << 8) | rx_data_[3]);  //单位 RPM
+  current = (int16_t)((rx_data_[4] << 8) | rx_data_[5]) * 20.f / 16384.f;  //单位 A
+  temperature = (int8_t)rx_data_[6];  //单位 ℃
 }
 
-M3508_Motor motor = M3508_Motor(3591/187);
+M3508_Motor motor = M3508_Motor(3591 / 187);
